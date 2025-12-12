2aeb91c4c0b66197a61b3138c3156d2a078e9dca
Shenura Mendis <94913388+shenuramendis@users.noreply.github.com>
2025-12-12 00:27:52 -0700

    Adding calculator.py

---
+++ calculator.py
@@ -0,0 +1,18 @@
+import math, numpy
+
+def read(exp): # reads expression to rpn stack
+    pass
+
+def push(item): # adds a number onto the expression stack
+    pass  
+
+def pop(item): # takes number out from expression stack
+    pass
+
+def main():
+    expression = str(input("Enter expression: "))
+    rpn_stack = read(expression)
+    pass
+
+if __name__ == "__main__":
+    main()
