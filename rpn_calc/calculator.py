07670734a1043f963ad6f35982f8319fe192f90a
Shenura Mendis <shenura.mendis@gmail.com>
2025-12-20 14:13:02 -0700

    update

--- rpn_calc/calculator.py
+++ rpn_calc/calculator.py
@@ -4,7 +4,7 @@ import rpn_func as rpn
 def main():
     expression = list(str(input("Enter expression: ")).replace(" ",""))
     rpn_stack = rpn.infix(expression)
-    print(rpn_stack)
+    #print(rpn_stack)
     print("Result: " + str(rpn.calculate(rpn_stack)))
 
 if __name__ == "__main__":
